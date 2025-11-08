5297ad432a3174e97bfe6dc252350d6cbf83f9c7
NormarieUPR <normarie.martinez@upr.edu>
2025-11-08 17:05:22 -0400

    Completed Task 5.1 Implementes a dictionary on the priceMap variable to keep track of the jokers prices

--- Deck/DeckManager.py
+++ Deck/DeckManager.py
@@ -154,8 +154,10 @@ class DeckManager:
         jokerImages = self.loadJokerImages()
         deckJokers = []
 
-        priceMap = {
-        }
+        priceMap = { 'The Joker' : 4, 'Micheal Myers' : 6 , 'Fibonacci' : 5, 'Gaunlet' : 5,
+                    'Orge' : 5, 'Straw Hat': 5, 'Hog Rider' : 4, '? Block' : 5, 'Hogwarts' : 6, '802' : 6 }
+
+
 
         for name, image in jokerImages.items():
             price = priceMap.get(name, 5)
