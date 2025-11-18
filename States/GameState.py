f91e70770f8cc99c6d010689a3b6fbd1aecdf542
juansfierro <j.fierro.pr@gmail.com>
2025-11-18 12:57:03 -0400

    Removed unnecesary code

--- States/GameState.py
+++ States/GameState.py
@@ -5,7 +5,7 @@ from States.Core.StateClass import State
 from Cards.Card import Suit, Rank
 from States.Core.PlayerInfo import PlayerInfo
 from Deck.HandEvaluator import evaluate_hand
-from testing.bonus import bonus, calculate_gold_rewards
+
 
 HAND_SCORES = {
     "Straight Flush": {"chips": 100, "multiplier": 8, "level": 1},
